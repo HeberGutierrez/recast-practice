5c3dd7fdd18e6e767161eda76194444fb14c0ec0
hebergutierrez.p@gmail.com <hebergutierrez.p@gmail.com>
2018-01-26 14:47:24 -0600

    First commit with sow data

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,10 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = () => (<div className="video-list">
+  <VideoListEntry/>
+  <VideoListEntry/>
+  <VideoListEntry/>
+  <VideoListEntry/>
+  <VideoListEntry/>
+</div>);
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
